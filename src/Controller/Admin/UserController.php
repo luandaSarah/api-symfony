c2299fac4c14d30ff4a969da47af920aedefe1ec
kaimon0 <luandasarah1031@gmail.com>
2025-05-20 15:34:29 +0200

    feat(udpate, delete) [ADD] user delete

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
+use App\Mapper\UserMapper;
 use App\Repository\UserRepository;
+use App\Dto\User\UpdateUserByAdminDto;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Json;
 
 #[Route('api/admin/users', 'api_admin_users_')] //Toute les routes commenceront par ca 
 class UserController extends AbstractController
@@ -15,6 +20,7 @@ class UserController extends AbstractController
     public function __construct(
         private EntityManagerInterface $em,
         private UserRepository $userRepository,
+        private readonly UserMapper $userMapper,
     ) {}
 
     #[Route('', name: 'index', methods: ['GET'])]
@@ -26,8 +32,40 @@ class UserController extends AbstractController
 
             //ici on defini des groupes afin de permettre la serialisation en JSON sur cette route specifique seulement sur les propriété qui sont accessible par les groupes (voir l'entité User)
             context: [
-                'groups' => ['common:index','users:index']
+                'groups' => ['common:index', 'users:index']
             ]
+        );
+    }
+
+    #[Route('/{id}', name: 'update', methods: ['PATCH'])]
+    public function update(
+        User $user,
+        #[MapRequestPayload]
+        UpdateUserByAdminDto $dto,
+    ): JsonResponse {
+
+        $this->userMapper->map($dto, $user);
+
+        $this->em->flush();
+
+        return $this->json(
+            $user,
+            Response::HTTP_OK,
+            context: [
+                'groups' => ['common:index', 'users:index', 'users:show']
+            ]
+        );
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(User $user): JsonResponse
+    {
+        $this->em->remove($user);
+        $this->em->flush();
+
+        return $this->json(
+            null,
+            Response::HTTP_NO_CONTENT,
         );
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -40,9 +40,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['users:show'])]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['users:show'])]
     private ?string $lastName = null;
 
 
