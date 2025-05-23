a731c85e083320327e850cdb1c5ebb59f8edf807
kaimon0 <luandasarah1031@gmail.com>
2025-05-23 17:08:43 +0200

    feat(pagination) [ADD]: mise en place de la pagination

--- src/Controller/Admin/ArticleController.php
+++ src/Controller/Admin/ArticleController.php
@@ -2,12 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Mapper\ArticleMapper;
 use App\Repository\ArticleRepository;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Dto\Article\CreateArticleByAdminDto;
+use App\Dto\Article\UpdateArticleByAdminDto;
+use App\Dto\Filter\ArticleFilterDto;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 
 //Endpoint principales
 #[Route('api/admin/articles', 'api_admin_articles_')]
@@ -16,23 +23,92 @@ class ArticleController extends AbstractController
 
     public function __construct(
         private ArticleRepository $articleRepository, //pour les requêtes haut niveau(lecture)
+        private readonly EntityManagerInterface $em, //les data ne peuvent etre modifier à ce moement là 
+        private readonly ArticleMapper $articleMapper,
     ) {}
 
     /**
      * Methode GET 
      * Récupere tout les Articles
-     *
      * @return JsonResponse
      */
     #[Route('', name: 'index', methods: ['GET'])]
-    public function index(): JsonResponse
-    {
+    public function index(
+        #[MapQueryString]
+        ArticleFilterDto $articleFilterDto, //transforme tout ce qu'il ya en paramettre GET de l'url après le ?
+    ): JsonResponse {
         return $this->json(
-            $this->articleRepository->findAll(),
+            $this->articleRepository->findPaginate($articleFilterDto),
             Response::HTTP_OK,
             context: [
-                'groups' => ['articles:index'],
+                'groups' => ['articles:index', 'common:index'],
             ]
+        );
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Article $article): JsonResponse
+    {
+        return $this->json(
+            $article,
+            Response::HTTP_OK,
+            context: [
+                'groups' => ['articles:show', 'articles:index', 'common:index']
+            ]
+        );
+    }
+
+    #[Route('', name: 'create', methods: ['POST'])]
+    public function create(
+        #[MapRequestPayload]
+        CreateArticleByAdminDto $dto
+    ): JsonResponse {
+        $article = $this->articleMapper->map($dto);
+
+        $this->em->persist($article); //On créer en bdd
+        $this->em->flush();
+
+        return $this->json(
+            [
+                'id' => $article->getId(),
+            ],
+            Response::HTTP_CREATED,
+            context: [
+                'groups' => ['articles:show'],
+            ]
+        );
+    }
+
+    #[Route('/{id}', name: 'update', methods: ['PATCH'])]
+    public function update(
+        Article $article,
+        #[MapRequestPayload]
+        UpdateArticleByAdminDto $dto,
+
+    ): JsonResponse {
+
+        $this->articleMapper->map($dto, $article);
+        $this->em->flush();
+
+        return $this->json(
+            $article,
+            Response::HTTP_OK,
+            context: [
+                'groups' => ['articles:show', 'common:index']
+            ]
+        );
+    }
+
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(Article $article): JsonResponse
+    {
+        $this->em->remove($article);
+        $this->em->flush();
+
+        return $this->json(
+            null, //on ne renvoie rien car on renvoie un statut http 204, no content
+            Response::HTTP_NO_CONTENT
         );
     }
 }
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -2,12 +2,15 @@
 
 namespace App\Repository;
 
+use App\Dto\Filter\ArticleFilterDto;
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * custom requete sql
  * @extends ServiceEntityRepository<Article>
+ * 
  */
 class ArticleRepository extends ServiceEntityRepository
 {
@@ -16,28 +19,62 @@ class ArticleRepository extends ServiceEntityRepository
         parent::__construct($registry, Article::class);
     }
 
-//    /**
-//     * @return Article[] Returns an array of Article objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+     * requete qui compte le nombre total du résultat 
+     * @return bool|float|int|string|null
+     */
+    public function countAll(): int
+    {
+        return $this->createQueryBuilder('a')
+            ->select('COUNT(a.id)')
+            ->getQuery()
+            ->getSingleScalarResult(); //renvoie juste un nombre
+    }
 
-//    public function findOneBySomeField($value): ?Article
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+     * Query 
+     * @param \App\Dto\Filter\ArticleFilterDto $filter
+     * @return array{items: mixed, meta: array{pages: float, total: bool|float|int|string|null}}
+     */
+    public function findPaginate(ArticleFilterDto $filter): array //retournera toujours un tableau vide
+    {
+        $offset = ($filter->getPage() - 1) * $filter->getLimit();
+        $query = $this->createQueryBuilder('a') //createQueryBuilder à partient à la class parente ServiceEntityRepository
+            ->setMaxResults($filter->getLimit())
+            ->setFirstResult($offset);
+
+        $total = $this->countAll();
+
+        return [
+            'items' => $query->getQuery()->getResult(), //resultat de la requetes
+            'meta' => [
+                'pages' => ceil($total / $filter->getLimit()),
+                'total' => $total
+            ] //information utile du resultat
+        ];
+    }
+    //    /**
+    //     * @return Article[] Returns an array of Article objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value) 
+    //            ->orderBy('a.id', 'ASC')
+    //            ->setMaxResults(10) //limit
+    //            ->getQuery() //transforme requette en chaine de caractere
+    //            ->getResult() //recupere le resultat de la requête sous forme de tableau
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Article
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
